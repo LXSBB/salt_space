d8dd5da53f8d592d24ee8b2bb8ce794e21f14f06
salt <1731798854@qq.com>
2022-04-10 22:13:35 +0800

    up

--- src/components/nav/logInBox.vue
+++ src/components/nav/logInBox.vue
@@ -5,27 +5,103 @@
       <img class="closeImg" src="src/assets/image/close.png" @click="closeLogInBox">
     </div>
     <div class="loginWays" >
-      <div class="mobileWays" v-showRipple="`rgba(113,164,183,0.3)`">
-        <span class="mobileWaysSpan">Log Ln With Mobile</span>
+      <div class="mobileWays" v-showRipple="`rgba(113,164,183,0.3)`" @click="changeWays">
+        <span class="mobileWaysSpan">{{isMobileWays?`Log Ln With Account`:`Log Ln With Mobile`}}</span>
       </div>
     </div>
     <div class="boxLine">
       Or
     </div>
+    <div class="loginForm">
+      <div class="loginFormWrap">
+        <transition name="fade">
+          <div class="userNameLogin loginFormContent" v-if="!isMobileWays">
+            <nav-input :inputType = '`账号/用户名/邮箱`'></nav-input>
+            <nav-input :inputType = '`密码`'></nav-input>
+          </div>
+        </transition>
+        <transition name="fade">
+          <div class="mobileLogin loginFormContent" v-if="isMobileWays">
+            <nav-input :inputType = '`大陆手机号码`'></nav-input>
+            <div class="sendCodeWrap">
+              <nav-input :inputType = '`验证码`'></nav-input>
+              <div class="sendCodeButton" @click="sendCode">
+                <span v-if="!isSendCode" class="sendCodeButtonSpan">Send</span>
+                <span v-else class="countDownSpan">{{countdown}}</span>
+              </div>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
+    <div class="loginWays" >
+      <div class="mobileWays loginSubmit" v-showRipple="`rgba(113,164,183,0.3)`">
+        <span class="mobileWaysSpan">Log Ln </span>
+      </div>
+    </div>
+    <div class="bottomLine"></div>
+    <div class="signUpButton">
+      <span>Sign Up</span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
+import {defineComponent, ref, nextTick} from 'vue';
 import bus from 'vue3-eventbus'
+import NavInput from "./navInput.vue";
 
 export default defineComponent({
+  components: {NavInput},
   setup() {
     function closeLogInBox() {
       bus.emit('closeLoginBox')
     }
+    let isMobileWays = ref(false)
+    //切换手机登录
+    function changeWays() {
+      isMobileWays.value = !isMobileWays.value
+      if(isSendCode.value) {
+        nextTick(() => {
+          let dom:any = document.querySelector('.sendCodeButton')
+          dom.style.backgroundColor = '#032B5288'
+          dom.style.cursor = 'not-allowed'
+        })
+      }
+    }
+    let isSendCode = ref(false)
+    //倒计时
+    let countdown = ref(10)
+    let timer:any
+    //发送验证码
+    function sendCode() {
+      if(isSendCode.value) {
+        return
+      }
+      isSendCode.value = true
+      let dom:any = document.querySelector('.sendCodeButton')
+      dom.style.backgroundColor = '#032B5288'
+      dom.style.cursor = 'not-allowed'
+      timer = setInterval(() => {
+        if(countdown.value > 0){
+          countdown.value --
+        }else{
+          console.log('===2=43=')
+          isSendCode.value = false
+          countdown.value = 10
+          dom.style.backgroundColor = '#0e3e98'
+          dom.style.cursor = 'pointer'
+          clearInterval(timer)
+        }
+      },1000)
+    }
     return {
-      closeLogInBox
+      closeLogInBox,
+      changeWays,
+      isMobileWays,
+      sendCode,
+      isSendCode,
+      countdown
     }
   }
 })
@@ -46,10 +122,10 @@ export default defineComponent({
   z-index: 2;
   top: 50%;
   left: 50%;
+  //color: rgba(3, 43, 82, 0.99);
   background-color: #f8f2f2;
   transform: translate(-50% ,-50%);
   width: 300px;
-  height: 450px;
   border-radius: 5%;
   box-shadow:  0 8px 24px rgb(119, 121, 122), 0 0 0  currentColor;
   display: flex;
@@ -65,6 +141,10 @@ export default defineComponent({
       width: 15px;
       height: 15px;
       cursor: pointer;
+      transition: all .2s;
+    }
+    .closeImg:hover{
+      transform:scale(1.2) ;
     }
   }
   .loginWays{
@@ -91,6 +171,10 @@ export default defineComponent({
         user-select: none;
       }
     }
+    .loginSubmit{
+      background-color: #4F35FDFF;
+      color: rgb(189, 217, 252);
+    }
     .mobileWays:hover{
       box-shadow:  0 8px 24px rgb(172, 175, 176), 0 0 0  currentColor;
       top: -5px;
@@ -104,6 +188,7 @@ export default defineComponent({
     font-size: 12px;
     color: #666666;
     user-select: none;
+    margin-bottom: 5px;
     &:before{
       width: 40%;
       height: 1px;
@@ -117,6 +202,66 @@ export default defineComponent({
       background-color: #666666;
     }
   }
+  .loginForm{
+    height: 120px;
+    .loginFormWrap{
+      width: 100%;
+      height: 100%;
+      position: relative;
+      .loginFormContent{
+        width: 100%;
+        position: absolute;
+        top: 0;
+        .sendCodeWrap{
+          display: flex;
+          justify-content: space-between;
+          .sendCodeButton{
+            width: 35%;
+            height: 50px;
+            border-radius: 10px;
+            background-color: #0e3e98;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            transition: all .3s;
+            .sendCodeButtonSpan{
+              user-select: none;
+              color: white;
+              font-size: 14px;
+            }
+            .countDownSpan{
+              user-select: none;
+              color: #d0cdcd;
+              font-size: 14px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .bottomLine{
+    background-color: #666666;
+    width: 100%;
+    height: 1px;
+    margin-top: 15px;
+  }
+  .signUpButton{
+    margin-top: 10px;
+    color: #4F35FDFF;
+    span:hover{
+      cursor: pointer;
+      text-decoration: underline;
+    }
+  }
 }
-
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(5%);
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all .2s ;
+}
 </style>
---
+++ src/components/nav/navInput.vue
@@ -0,0 +1,63 @@
+<template>
+  <div :class="{'navInputContainer':true,'inputFocus':isFocus,'codeWidth':smallSize}" >
+    <input @focus="isFocus = true" @blur="isFocus = false" class="navInput" :placeholder="props.inputType"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, onMounted, toRefs, ref} from 'vue';
+
+export default defineComponent({
+  props:{
+    inputType:String
+  },
+  setup(props) {
+    const {inputType} = toRefs(props)
+    let isFocus = ref(false)
+    function inputFocus() {
+
+    }
+    const smallSize = ref(false)
+    onMounted(() => {
+      if(inputType.value === '验证码') {
+        smallSize.value = true
+      }
+    })
+    return {
+      props,
+      inputFocus,
+      isFocus,
+      smallSize
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.navInputContainer{
+  width: 100%;
+  height: 50px;
+  border-radius: 10px;
+  border: 1px solid rgb(24, 123, 246);
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  margin-bottom: 10px;
+  .navInput{
+    width: 100%;
+    font-size: 14px;
+    line-height: normal;
+    color: #666666;
+  }
+  input::-webkit-input-placeholder {
+    color: #aab2bd;
+    font-size: 14px;
+  }
+}
+.inputFocus{
+  box-shadow:  0 0 0 3px rgba(3,102,214,0.3);
+}
+.codeWidth {
+  width: 60%;
+}
+</style>
